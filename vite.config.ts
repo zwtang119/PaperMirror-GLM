1c3fdb9040723ea0c032283082599e43022984e9
zwtang119 <zwtang119@gmail.com>
2025-12-07 23:15:42 +0800

    v0.3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/PaperMirror/',
+  base: '/PaperMirror-GLM/',
   
   build: {
     outDir: 'dist',
